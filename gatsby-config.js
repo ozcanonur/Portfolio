9ee538b9a4d02b5e821e83441515c1c57384de5a
ozcanonur <ozcanonur10@gmail.com>
2021-01-16 15:53:25 +0000

    Misc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'www.onurozcan.io',
+    siteUrl: 'https://www.onurozcan.io',
     title: 'Onur Ozcan',
     description: 'Onur Ozcan personal portfolio website.',
-    url: 'www.onurozcan.io',
+    url: 'https://www.onurozcan.io',
   },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-robots-txt`, `gatsby-plugin-react-helmet`, `gatsby-plugin-smoothscroll`],
 };
